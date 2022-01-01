81cdac1e352ed35adead27eff7b29a3d3d58644e
Simon Dückert <simon.dueckert@cogneon.de>
2022-01-01 16:05:28 +0100

    Kapitel Github hinzugefügt

--- de/make.sh
+++ de/make.sh
@@ -3,7 +3,7 @@ echo Starting lernOS Guide Generation ...
 
 # Variables
 filename="lernOS-Template-Guide-de"
-chapters="./src/index.md ./src/1-0-Grundlagen.md ./src/1-1-Grundidee.md ./src/1-2-Lebenszyklus.md ./src/1-3-Inhaltsverzeichnis.md ./src/1-4-Inhalt-Schreibstil.md ./src/1-5-Markdown-Syntax.md ./src/1-6-Produktionskette.md ./src/2-0-Lernpfad.md ./src/3-0-Anhang.md"
+chapters="./src/index.md ./src/1-0-Grundlagen.md ./src/1-1-Grundidee.md ./src/1-2-Lebenszyklus.md ./src/1-3-Inhaltsverzeichnis.md ./src/1-4-Inhalt-Schreibstil.md ./src/1-5-Markdown-Syntax.md ./src/1-6-Produktionskette.md ./src/1-7-Github.md ./src/2-0-Lernpfad.md ./src/3-0-Anhang.md"
 
 # Delete Old Versions
 echo Deleting old versions ...

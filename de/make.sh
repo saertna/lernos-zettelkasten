f1ac61f398b77db8ae0454435156a90a943d480a
Simon Dückert <simon.dueckert@cogneon.de>
2022-01-02 23:54:00 +0100

    TOC hinzugefügt

--- de/make.sh
+++ de/make.sh
@@ -1,4 +1,4 @@
-PATH=$PATH:/opt/homebrew/bin
+#PATH=$PATH:/opt/homebrew/bin
 echo Starting lernOS Guide Generation ...
 
 # Variables
@@ -17,24 +17,28 @@ mkdocs build
 
 # Create Microsoft Word Version (docx)
 echo Creating Word version ...
-pandoc metadata.yaml --from markdown -s --resource-path="./src" -F mermaid-filter --number-sections -V lang=de-de -o $filename.docx $chapters
+pandoc metadata.yaml -s --resource-path="./src" -F mermaid-filter -N --toc -V lang=de-de -o $filename.docx $chapters
 
 # Create HTML Version (html)
 echo Creating HTML version ...
-pandoc metadata.yaml --from markdown -s --resource-path="./src" -F mermaid-filter --number-sections -V lang=de-de -o $filename.html $chapters
+pandoc metadata.yaml -s --resource-path="./src" -F mermaid-filter -N --toc -V lang=de-de -o $filename.html $chapters
 
 # Create PDF Version (pdf)
 echo Creating PDF version ...
-pandoc metadata.yaml --from markdown -s --resource-path="./src" -F mermaid-filter --template lernos --number-sections --toc -V lang=de-de -o $filename.pdf $chapters
+pandoc metadata.yaml -s --resource-path="./src" -F mermaid-filter --template lernos -N --toc -V lang=de-de -o $filename.pdf $chapters
 
 # Create eBook Versions (epub, mobi)
 echo Creating eBook versions ...
-magick -density 300 $filename.pdf[0] src/images/ebook-cover.jpg
-mogrify -size 2500x2500 -resize 2500x2500 src/images/ebook-cover.jpg
-mogrify -crop 1563x2500+102+0 src/images/ebook-cover.jpg
-pandoc metadata.yaml --from markdown -s --resource-path="./src" -F mermaid-filter --epub-cover-image=src/images/ebook-cover.jpg --number-sections --toc -V lang=de-de -o $filename.epub $chapters
+magick -density 300 $filename.pdf[0] ebook-cover.jpg
+mogrify -size 2500x2500 -resize 2500x2500 ebook-cover.jpg
+mogrify -crop 1563x2500+102+0 ebook-cover.jpg
+pandoc metadata.yaml -s --resource-path="./src" -F mermaid-filter --epub-cover-image=ebook-cover.jpg -N --toc -V lang=de-de -o $filename.epub $chapters
 ebook-convert $filename.epub $filename.mobi
 
 # Create Slides (revealjs)
 # echo Creating Presentation ...
 # pandoc metadata.yaml --from markdown -s --resource-path="./src" -t revealjs -V theme=night -s ./slides/index.md -o ../docs/de-slides/index.html
+
+# Remove Unnecessary Files
+rm mermaid-filter.err
+rm ebook-cover.jpg
